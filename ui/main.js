ca14062ce305df25ae7a826e8ceb8e24a9ce6e43
Burhanuddin Khomosi <burhan.khomosi@gmail.com>
2016-09-23 19:48:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var img= document.getElementById("madi");
 var marginLeft=0;
 function moveRight(){
     marginLeft=marginLeft+5;
-    if(marginLeft<=100)
+    if(marginLeft<=550)
     img.style.marginLeft = marginLeft+"px";
 }
 
