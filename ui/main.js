21e0936854518ac44484caddc430a4982206ff92
Burhanuddin Khomosi <burhan.khomosi@gmail.com>
2016-09-23 18:32:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var img= document.getElementById("myImage");
 var marginLeft=0;
 function moveRight(){
     marginLeft = marginLeft+5;
-    img.style.marginLeft = marginLeft+"px";
+    img.style.marginLeft = ""+marginLeft+"px";
 }
 
 img.onclick = function(){
